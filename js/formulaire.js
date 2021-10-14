ed422f4dd7b5b56830af1a31bdb1937d2119d837
Laurina Toussaint <laurinatoussaint@gmail.com>
2021-10-14 15:23:46 +0200

    vérifications des inputs, chaud patate

--- js/formulaire.js
+++ js/formulaire.js
@@ -1,24 +1,31 @@
 document. getElementById("contact").addEventListener("submit", function(e) {
+    e.preventDefault();
     let erreur;
     
     let nom = document.getElementById("nom");
     let prenom = document.getElementById("prenom");
     let telephone = document.getElementById("telephone");
+    let choice = document.getElementsByClassName("choice.value");
+    console.log(choice);
 
-    if(!telephone.value) {
+    // if(textarea.required = false) {
+    //     erreur = "Votretexte est trop long";
+    // }
+
+    if(!telephone.value.length == 10 ) {
         erreur = "Votre numéro de téléphone n'est pas valide";
     }
 
-    if(!prenom.value) {
+    if(!prenom.value || prenom.value.lenght < 2 || prenom.value.length > 10) {
         erreur = "Votre prénom n'est pas valide";
     }
 
-    if(!nom.value) {
+    if(!nom.value || nom.value.lenght < 2 || nom.value.length > 10) {
         erreur = "Votre nom n'est pas valide";
     }
-    if(textarea.length > 200) {
-        erreur = "Votretexte est trop long";
-    }
+    // if(textarea.length > 200) {
+    //     erreur = "Votretexte est trop long";
+    // }
   
     if (erreur) {
         e.preventDefault();
